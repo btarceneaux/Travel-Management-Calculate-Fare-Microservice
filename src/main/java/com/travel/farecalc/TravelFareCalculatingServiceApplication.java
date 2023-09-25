d4fbbe8baafbefcab8cff209929e1992aaa2c1b0
Burton Arceneaux <btarceneaux@gmail.com>
2023-09-24 20:54:15 -0700

    Initial commot of gitignore

---
+++ src/main/java/com/travel/farecalc/TravelFareCalculatingServiceApplication.java
@@ -0,0 +1,13 @@
+package com.travel.farecalc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelFareCalculatingServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TravelFareCalculatingServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/travel/farecalc/TravelFareCalculatingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel.farecalc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelFareCalculatingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
